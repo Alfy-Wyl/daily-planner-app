4ce2b88e558e0015f7a9201476d9b366056296a3
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-01-28 16:00:50 +0000

    added code to app.js file

--- assests/js/app.js
+++ assests/js/app.js
@@ -4,5 +4,13 @@ $(document).ready(function(){
     // Add event listener to listen for the Save Button Click
     $(".saveBtn").on("click", function(){
 
+        // Set variables to be saved in local storage
+        let hourID = $(this).parent().attr('id')
+        let value = $(this).siblings(".description").val();
+
+        localStorage.setItem(hourID, value);
+
+        $(".notification").addClass("show");
+
     })
 }) 
