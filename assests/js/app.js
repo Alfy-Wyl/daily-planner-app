e8aa6db80a0477e2899d88168019f41eb07bad29
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-01-28 14:47:00 +0000

    added code to app.js file

---
+++ assests/js/app.js
@@ -0,0 +1,8 @@
+// Create function to store the different values user inputs
+$(document).ready(function(){
+
+    // Add event listener to listen for the Save Button Click
+    $(".saveBtn").on("click", function(){
+
+    })
+}) 
